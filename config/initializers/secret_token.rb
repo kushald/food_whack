508348cc88621968b480c99bd65f8be2fc4c694d
Kushal Dongre <kushz19@gmail.com>
2013-12-09 21:10:00 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoodWhack::Application.config.secret_key_base = 'aff071c64ddd62a6e96456968e257e2587ea231ad6a2ae46212c1466a2634b9a814c3d052a3651f726376c6ed9741092e47868e23611e440c79951602af44314'
